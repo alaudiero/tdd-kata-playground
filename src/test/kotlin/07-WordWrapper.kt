63eb240884542cf88a91ffa8c9bf6eb9c62e3024
Aurelio Laudiero <32538884+Huginn87@users.noreply.github.com>
2020-07-26 13:01:31 +0200

    added test for empty input

--- src/test/kotlin/07-WordWrapper.kt
+++ src/test/kotlin/07-WordWrapper.kt
@@ -1,3 +1,6 @@
+import org.junit.Test
+import org.junit.jupiter.api.assertThrows
+
 /**
 Word wrapper
 You write a class called Wrapper, that has a single static function named wrap that takes two arguments,
@@ -7,6 +10,30 @@ right places to make sure that no line is longer than the column number. You try
 Like a word processor, break the line by replacing the last space in a line with a newline.
  **/
 
+/**
+ *   "", any number -> rise error
+ *   " ", any number -> rise error
+ *   "hello, world!", 10 -> "hello, world!"
+ *   "hello, world!", 5 -> "hello,\nworld!"
+ *   "Like a word processor, break the line by replacing the last space in a line with a newline.", 25 ->
+ *      "Like a word processor, break\nthe line by replacing the\nlast space in a line with\na newline."
+ */
+
 class WorkWrapperTest {
 
+    @Test
+    fun test0() {
+        assertThrows<IllegalArgumentException>() { Wrapper.wrap("", 42) }
+    }
+}
+
+class Wrapper {
+    companion object {
+        fun wrap(text: String, column: Int): String {
+            if (text.isEmpty()) throw IllegalArgumentException()
+            else {
+                return ""
+            }
+        }
+    }
 }
